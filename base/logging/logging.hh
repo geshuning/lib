7f3e5734d56616b25d17f8ca8bcfd5dfa3276a1e
geshuning <gsnsheqiu@163.com>
2014-09-10 18:31:45 +0800

    Modify LICENSE

--- base/logging/logging.hh
+++ base/logging/logging.hh
@@ -1,10 +1,25 @@
-// Copyright(c)2013,nsfocus
-// All rights reserved.
-// Author: Ge Shuning<geshuning@intra.nsfocus.com>
-//
+// Copyright (c) 2014 Shuning Ge
 
-#ifndef NF_BASE_LOGGING_LOGGING_H_
-#define NF_BASE_LOGGING_LOGGING_H_
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef BASE_LOGGING_LOGGING_HH_
+#define BASE_LOGGING_LOGGING_HH_
 
 #include <string>
 #include <vector>
@@ -179,4 +194,4 @@ namespace base {
 
 }  // namespace base
 #include "base/logging/check.hh"
-#endif  // NF_BASE_LOGGING_LOGGING_H_
+#endif  // BASE_LOGGING_LOGGING_HH_
